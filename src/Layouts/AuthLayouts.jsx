b8dfac0ce39094eaf5189163753910de3114472f
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-12-11 00:02:22 +0600

    resolve

---
+++ src/Layouts/AuthLayouts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../Components/Logo";
+
+const AuthLayouts = () => {
+  return (
+    <div>
+      <div>
+        <Logo></Logo>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayouts;
