83e11be5512e8550af87e58e4d9054e9f652d4ac
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-12-11 15:58:32 +0600

    add a registar

--- src/Components/Registar.jsx
+++ src/Components/Registar.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Registar = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handelRegister = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <h2>This is Registar</h2>
+      <form onSubmit={handleSubmit(handelRegister)} className="fieldset">
+        <label className="label">Email</label>
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          className="input"
+          placeholder="Email"
+        />
+        {errors.email && <p className="text-red-500">Email is required</p>}
+        <label className="label">Password</label>
+        <input
+          type="password"
+          {...register("password", { required: true, minLength: 6 })}
+          className="input"
+          placeholder="Password"
+        />
+        {errors.password && (
+          <p className="text-red-500">Password is required</p>
+        )}
+        <button type="submit" className="btn btn-neutral mt-4">
+          Login
+        </button>
+      </form>
     </div>
   );
 };
